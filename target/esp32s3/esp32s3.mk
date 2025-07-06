c18d2a31151ca4dadf24eed21217e5b85a69fc1e
xRuuul <raoul.dittmann@gmail.com>
2025-07-06 16:13:49 +0200

    Update esp-idf version to v5.4.2 and remove single-thread compile patch

--- target/esp32s3/esp32s3.mk
+++ target/esp32s3/esp32s3.mk
@@ -6,8 +6,8 @@ define esp32s3/info
 	PKG_NAME:=esp-idf
 	PKG_SOURCE_URL:=https://github.com/espressif/esp-idf.git
 	PKG_SOURCE_PROTO:=git
-	PKG_SOURCE_VERSION:=v4.4.4
-	PKG_SOURCE_MD5:=9416da50831df089fcf60db5fc2750f0
+	PKG_SOURCE_VERSION:=v5.4.2
+	PKG_SOURCE_MD5:=
 	PKG_SOURCE_OPT:=--depth 1 --branch v4.4.4 --recurse-submodules
 endef
 
